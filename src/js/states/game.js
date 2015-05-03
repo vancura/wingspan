3cae47ef875e62736e0b78adaec010a0a9578a23
Vaclav Vancura <vaclav@vancura.org>
2015-05-03 15:05:51 -0700

    Physics tweaks, now with backpedal [S]

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -9,9 +9,12 @@
     };
 
 
-    GameState.WORLD_GRAVITY = 20;
+    GameState.WORLD_GRAVITY = 40;
     GameState.WORLD_OVERFLOW = -10;
-    GameState.PLANE_THRUST = 90;
+    GameState.PLANE_MAX_THRUST = 90;
+    GameState.PLANE_THRUST_MULTIPLIER_UP = 1.9; // thrust multiplier when thrust button down
+    GameState.PLANE_THRUST_MULTIPLIER_DOWN = 0.5; // thrust multiplier when backpedal button down
+    GameState.PLANE_THRUST_MULTIPLIER_NONE = 0.995; // thrust step when both thrust and backpedal buttons released
     GameState.PLANE_LINEAR_DAMPING = 1;
     GameState.PLANE_ANGULAR_DAMPING_FACTOR = 20;
     GameState.PLANE_KEYBOARD_ROTATION_STEP = 70;
@@ -30,8 +33,12 @@
             this.mouseStep = 0;
 
             // current rotation degree; 0 = absolutely controllable, abs(1) = lost control
-            // could be -1..+1 depending on rotation direction
-            this.planeControlDegree = 0;
+            // could be -1 .. +1 depending on rotation direction
+            this.currentPlaneControlDegree = 0;
+
+            // current thrust
+            // could be 0.1 .. PLANE_MAX_THRUST
+            this.currentPlaneThrust = 0.1;
 
             this.physics.startSystem(Phaser.Physics.BOX2D);
             this.physics.box2d.setBoundsToWorld(true, true, false, true);
@@ -54,7 +61,7 @@
          * Render.
          */
         render: function () {
-            this.game.debug.box2dWorld();
+            //this.game.debug.box2dWorld();
 
             //if (this.plane) {
             //this.debug.bodyInfo(this.plane, this.plane.x + 20, this.plane.y + 20);
@@ -71,23 +78,23 @@
             if (this.plane) {
                 if (this.leftButton.isDown && !this.rightButton.isDown) {
                     // rotating left, increase rotation degree until it's +1
-                    this.planeControlDegree += GameState.PLANE_CONTROL_DEGREE_STEP;
+                    this.currentPlaneControlDegree += GameState.PLANE_CONTROL_DEGREE_STEP;
                 }
                 else if (!this.leftButton.isDown && this.rightButton.isDown) {
                     // rotating right, decrease rotation degree until it's -1
-                    this.planeControlDegree -= GameState.PLANE_CONTROL_DEGREE_STEP;
+                    this.currentPlaneControlDegree -= GameState.PLANE_CONTROL_DEGREE_STEP;
                 }
 
                 // clamp rotation degree to -1..+1
-                this.planeControlDegree = Phaser.Math.clamp(this.planeControlDegree, -1, 1);
+                this.currentPlaneControlDegree = Phaser.Math.clamp(this.currentPlaneControlDegree, -1, 1);
 
                 // prevent division by zero below
-                if (this.planeControlDegree === 0) {
-                    this.planeControlDegree = 0.001;
+                if (this.currentPlaneControlDegree === 0) {
+                    this.currentPlaneControlDegree = 0.001;
                 }
 
                 // calculate new rotation
-                var rot = GameState.PLANE_KEYBOARD_ROTATION_STEP * this.planeControlDegree;
+                var rot = GameState.PLANE_KEYBOARD_ROTATION_STEP * this.currentPlaneControlDegree;
 
                 // tweak based on plane speed
                 // the faster plane goes the more difficult is to control it
@@ -95,15 +102,29 @@
                 var vel = Math.sqrt(this.plane.body.velocity.x * this.plane.body.velocity.x + this.plane.body.velocity.y * this.plane.body.velocity.y);
 
                 // apply the angular damping from velocity calculated above
-                this.plane.body.angularDamping = vel / GameState.PLANE_ANGULAR_DAMPING_FACTOR / this.planeControlDegree;
+                this.plane.body.angularDamping = vel / GameState.PLANE_ANGULAR_DAMPING_FACTOR / this.currentPlaneControlDegree;
 
                 // and finally rotate the plane
                 this.plane.body.rotateLeft(rot);
 
-                // thrust if thrust key pressed
                 if (this.thrustButton.isDown) {
-                    this.plane.body.thrust(GameState.PLANE_THRUST);
+                    // thrust button down, thrust up
+                    this.currentPlaneThrust *= GameState.PLANE_THRUST_MULTIPLIER_UP;
                 }
+                else if (this.backpedalButton.isDown) {
+                    // backpedal button down, thrust down
+                    this.currentPlaneThrust *= GameState.PLANE_THRUST_MULTIPLIER_DOWN;
+                }
+                else {
+                    // thrust button released and no backpedal down,
+                    // slowly decrease thrust
+                    this.currentPlaneThrust *= GameState.PLANE_THRUST_MULTIPLIER_NONE;
+                }
+
+                // apply current thrust
+                this.currentPlaneThrust = Phaser.Math.clamp(this.currentPlaneThrust, 0.1, GameState.PLANE_MAX_THRUST);
+                console.log(this.currentPlaneThrust);
+                this.plane.body.thrust(this.currentPlaneThrust);
 
                 // draw trails, calculate the distance multiplier
                 this.drawTrails(1 - Math.abs(rot / 70));
@@ -181,6 +202,7 @@
             this.leftButton = this.input.keyboard.addKey(Phaser.Keyboard.A);
             this.rightButton = this.input.keyboard.addKey(Phaser.Keyboard.D);
             this.thrustButton = this.input.keyboard.addKey(Phaser.Keyboard.W);
+            this.backpedalButton = this.input.keyboard.addKey(Phaser.Keyboard.S);
 
             this.input.onDown.add(this.onDown, this);
             this.input.onUp.add(this.onUp, this);
