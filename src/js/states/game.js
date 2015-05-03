4b121b079c9898a6fddc328b60d9c582e0bc1a97
Vaclav Vancura <vaclav@vancura.org>
2015-05-02 20:40:29 -0700

    Plane trails

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -15,18 +15,27 @@
         init: function () {
             this.physics.startSystem(Phaser.Physics.BOX2D);
             this.physics.box2d.setBoundsToWorld();
-            this.physics.box2d.gravity.y = 200;
+            //this.physics.box2d.gravity.y = 200;
             this.physics.box2d.restitution = 0.4;
         },
 
 
         create: function () {
+            this.trailGraphics1 = this.add.graphics(0, 0);
+            this.trailGraphics2 = this.add.graphics(0, 0);
+
             this.plane = this.add.sprite(this.world.centerX, this.world.centerY, "game", "plane.png");
             this.plane.anchor.set(0.5, 0.5);
+            this.plane.angle = 90;
 
             this.physics.box2d.enable(this.plane);
 
-            this.plane.body.setCircle(this.plane.width / 2.8);
+            this.plane.body.setCircle(this.plane.width / 2);
+
+            var pos = this.getTrailsPosition();
+
+            this.trailGraphics1.moveTo(pos[0], pos[1]);
+            this.trailGraphics2.moveTo(pos[2], pos[3]);
 
             this.leftButton = this.input.keyboard.addKey(Phaser.Keyboard.A);
             this.rightButton = this.input.keyboard.addKey(Phaser.Keyboard.D);
@@ -34,7 +43,7 @@
 
 
         render: function () {
-            game.debug.box2dWorld();
+            // game.debug.box2dWorld();
         },
 
 
@@ -48,7 +57,30 @@
                 }
 
                 this.plane.body.thrust(200);
+
+                var pos = this.getTrailsPosition();
+
+                this.trailGraphics1.lineStyle(1, 0xFF0000, 0.5);
+                this.trailGraphics1.lineTo(pos[0], pos[1]);
+
+                this.trailGraphics2.lineStyle(1, 0xFF0000, 0.5);
+                this.trailGraphics2.lineTo(pos[2], pos[3]);
             }
+        },
+
+
+        // PRIVATE
+
+
+        getTrailsPosition: function () {
+            var d = this.plane.rotation * -1 - 90 * (Math.PI / 180);
+
+            var mx1 = Math.sin(d) * -20 + this.plane.x;
+            var my1 = Math.cos(d) * -20 + this.plane.y;
+            var mx2 = Math.sin(d) * 20 + this.plane.x;
+            var my2 = Math.cos(d) * 20 + this.plane.y;
+
+            return [mx1, my1, mx2, my2];
         }
 
 
