6a6f089138dbf526959c1f21d22bc1374a2c0610
Vaclav Vancura <vaclav@vancura.org>
2015-05-02 19:05:09 -0700

    Basic plane physics

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -14,7 +14,9 @@
 
         init: function () {
             this.physics.startSystem(Phaser.Physics.BOX2D);
-            this.physics.box2d.restitution = 0.1;
+            this.physics.box2d.setBoundsToWorld();
+            this.physics.box2d.gravity.y = 200;
+            this.physics.box2d.restitution = 0.4;
         },
 
 
@@ -25,10 +27,28 @@
             this.physics.box2d.enable(this.plane);
 
             this.plane.body.setCircle(this.plane.width / 2.8);
+
+            this.leftButton = this.input.keyboard.addKey(Phaser.Keyboard.A);
+            this.rightButton = this.input.keyboard.addKey(Phaser.Keyboard.D);
+        },
+
+
+        render: function () {
+            game.debug.box2dWorld();
         },
 
 
         update: function () {
+            if (this.plane) {
+                if (this.leftButton.isDown && !this.rightButton.isDown) {
+                    this.plane.body.rotateLeft(50);
+                }
+                else if (!this.leftButton.isDown && this.rightButton.isDown) {
+                    this.plane.body.rotateRight(50);
+                }
+
+                this.plane.body.thrust(200);
+            }
         }
 
 
