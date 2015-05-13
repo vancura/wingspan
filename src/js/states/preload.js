748b0d88151513971e83a330c553f74513a68724
Vaclav Vancura <vaclav@vancura.org>
2015-05-12 21:00:20 -0700

    Assets optimization

--- src/js/states/preload.js
+++ src/js/states/preload.js
@@ -21,11 +21,11 @@
             // this.load.crossOrigin = 'anonymous';
 
             this.load.atlasJSONHash("game", "assets/game.png", "assets/game.json");
-            this.load.audio("engineLoop", "assets/audio/base-engine-loop.wav");
-            this.load.audio("engineStress", "assets/audio/base-engine-stress.wav");
-            this.load.audio("gunshot", "assets/audio/gunshot.wav");
-            this.load.audio("explosion", "assets/audio/explosion.wav");
-            this.load.audio("music-parapet", "assets/audio/music-parapet.wav");
+            this.load.audio("engineLoop", ["assets/audio/base-engine-loop.mp3", "assets/audio/base-engine-loop.ogg"]);
+            this.load.audio("engineStress", ["assets/audio/base-engine-stress.mp3", "assets/audio/base-engine-stress.ogg"]);
+            this.load.audio("gunshot", ["assets/audio/gunshot.mp3", "assets/audio/gunshot.ogg"]);
+            this.load.audio("explosion", ["assets/audio/explosion.mp3", "assets/audio/explosion.ogg"]);
+            this.load.audio("music-parapet", ["assets/audio/music-parapet.mp3", "assets/audio/music-parapet.ogg"]);
             this.load.image("forestBackground", "assets/forest-bg-1920x1080.jpg");
         },
 
