bcde8e82594a1d44e21fec734b374f9f0d5a1b90
Vaclav Vancura <vaclav@vancura.org>
2015-05-28 15:47:11 +0200

    Minor GUI optimization

--- src/ts/objects/GUI.class.ts
+++ src/ts/objects/GUI.class.ts
@@ -20,71 +20,40 @@ class GUI extends Phaser.Group {
 
         super(game, game.world, "gui");
 
-        this.scenicSingleModeButton               = new Phaser.Button(this.game, x, this.game.world.height, "game", this.switchGameModeState, this, "gui/scenic-single-over.png", "gui/scenic-single-out.png");
-        this.scenicSingleModeButton.fixedToCamera = true;
-        this.scenicSingleModeButton.anchor.y      = 1;
-
-        this.scenicSingleModeLabel               = new Phaser.Image(this.game, x, this.game.world.height, "game", "gui/scenic-single-active.png");
-        this.scenicSingleModeLabel.fixedToCamera = true;
-        this.scenicSingleModeLabel.anchor.y      = 1;
-        this.scenicSingleModeLabel.visible       = false;
-
-        x += this.scenicSingleModeButton.width + 5;
-
-        this.local2PlayersModeButton               = new Phaser.Button(this.game, x, this.game.world.height, "game", this.switchGameModeState, this, "gui/local-2-players-over.png", "gui/local-2-players-out.png");
-        this.local2PlayersModeButton.fixedToCamera = true;
-        this.local2PlayersModeButton.anchor.y      = 1;
-
-        this.local2PlayersModeLabel               = new Phaser.Image(this.game, x, this.game.world.height, "game", "gui/local-2-players-active.png");
-        this.local2PlayersModeLabel.fixedToCamera = true;
-        this.local2PlayersModeLabel.anchor.y      = 1;
-        this.local2PlayersModeLabel.visible       = false;
-
-        x += this.local2PlayersModeButton.width + 5;
-
-        this.remoteXPlayersModeButton               = new Phaser.Button(this.game, x, this.game.world.height, "game", this.switchGameModeState, this, "gui/remote-x-players-over.png", "gui/remote-x-players-out.png");
-        this.remoteXPlayersModeButton.fixedToCamera = true;
-        this.remoteXPlayersModeButton.anchor.y      = 1;
-
-        this.remoteXPlayersModeLabel               = new Phaser.Image(this.game, x, this.game.world.height, "game", "gui/remote-x-players-active.png");
-        this.remoteXPlayersModeLabel.fixedToCamera = true;
-        this.remoteXPlayersModeLabel.anchor.y      = 1;
-        this.remoteXPlayersModeLabel.visible       = false;
-
-        this.add(this.scenicSingleModeButton);
-        this.add(this.scenicSingleModeLabel);
-        this.add(this.local2PlayersModeButton);
-        this.add(this.local2PlayersModeLabel);
-        this.add(this.remoteXPlayersModeButton);
-        this.add(this.remoteXPlayersModeLabel);
+        x += this.createScenicSingleModeButton(x) + 5;
+        x += this.createLocal2PlayersModeButton(x) + 5;
+        x += this.createRemoteXPlayersModeButton(x) + 5;
 
         this.refresh();
     }
 
 
+    /**
+     * Refresh GUI.
+     */
     public refresh() {
-        this.scenicSingleModeButton.buttonMode   = true;
-        this.local2PlayersModeButton.buttonMode  = true;
+        this.scenicSingleModeButton.buttonMode = true;
+        this.local2PlayersModeButton.buttonMode = true;
         this.remoteXPlayersModeButton.buttonMode = true;
 
-        this.scenicSingleModeLabel.visible   = false;
-        this.local2PlayersModeLabel.visible  = false;
+        this.scenicSingleModeLabel.visible = false;
+        this.local2PlayersModeLabel.visible = false;
         this.remoteXPlayersModeLabel.visible = false;
 
         switch (GameState.gameModeState) {
             case GameModeState.ScenicSingle:
                 this.scenicSingleModeButton.inputEnabled = false;
-                this.scenicSingleModeLabel.visible       = true;
+                this.scenicSingleModeLabel.visible = true;
                 break;
 
             case GameModeState.Local2Players:
                 this.local2PlayersModeButton.inputEnabled = false;
-                this.local2PlayersModeLabel.visible       = true;
+                this.local2PlayersModeLabel.visible = true;
                 break;
 
             case GameModeState.RemoteXPlayers:
                 this.remoteXPlayersModeButton.inputEnabled = false;
-                this.remoteXPlayersModeLabel.visible       = true;
+                this.remoteXPlayersModeLabel.visible = true;
                 break;
         }
     }
@@ -92,6 +61,72 @@ class GUI extends Phaser.Group {
 
     // PRIVATE
     // -------
+
+
+    /**
+     * Create the scenic single mode button
+     * @param x Current x position
+     * @return {any} Length
+     */
+    private createScenicSingleModeButton(x:number) {
+        this.scenicSingleModeButton = new Phaser.Button(this.game, x, this.game.world.height, "game", this.switchGameModeState, this, "gui/scenic-single-over.png", "gui/scenic-single-out.png");
+        this.scenicSingleModeButton.fixedToCamera = true;
+        this.scenicSingleModeButton.anchor.y = 1;
+
+        this.scenicSingleModeLabel = new Phaser.Image(this.game, x, this.game.world.height, "game", "gui/scenic-single-active.png");
+        this.scenicSingleModeLabel.fixedToCamera = true;
+        this.scenicSingleModeLabel.anchor.y = 1;
+        this.scenicSingleModeLabel.visible = false;
+
+        this.add(this.scenicSingleModeButton);
+        this.add(this.scenicSingleModeLabel);
+
+        return this.scenicSingleModeButton.width;
+    }
+
+
+    /**
+     * Create the local two players mode button.
+     * @param x Current x position
+     * @return {number} Length
+     */
+    private createLocal2PlayersModeButton(x:number) {
+        this.local2PlayersModeButton = new Phaser.Button(this.game, x, this.game.world.height, "game", this.switchGameModeState, this, "gui/local-2-players-over.png", "gui/local-2-players-out.png");
+        this.local2PlayersModeButton.fixedToCamera = true;
+        this.local2PlayersModeButton.anchor.y = 1;
+
+        this.local2PlayersModeLabel = new Phaser.Image(this.game, x, this.game.world.height, "game", "gui/local-2-players-active.png");
+        this.local2PlayersModeLabel.fixedToCamera = true;
+        this.local2PlayersModeLabel.anchor.y = 1;
+        this.local2PlayersModeLabel.visible = false;
+
+        this.add(this.local2PlayersModeButton);
+        this.add(this.local2PlayersModeLabel);
+
+        return this.local2PlayersModeButton.width;
+    }
+
+
+    /**
+     * Create the remote x players mode button.
+     * @param x Current x position
+     * @return {number} Length
+     */
+    private createRemoteXPlayersModeButton(x:number) {
+        this.remoteXPlayersModeButton = new Phaser.Button(this.game, x, this.game.world.height, "game", this.switchGameModeState, this, "gui/remote-x-players-over.png", "gui/remote-x-players-out.png");
+        this.remoteXPlayersModeButton.fixedToCamera = true;
+        this.remoteXPlayersModeButton.anchor.y = 1;
+
+        this.remoteXPlayersModeLabel = new Phaser.Image(this.game, x, this.game.world.height, "game", "gui/remote-x-players-active.png");
+        this.remoteXPlayersModeLabel.fixedToCamera = true;
+        this.remoteXPlayersModeLabel.anchor.y = 1;
+        this.remoteXPlayersModeLabel.visible = false;
+
+        this.add(this.remoteXPlayersModeButton);
+        this.add(this.remoteXPlayersModeLabel);
+
+        return this.remoteXPlayersModeButton.width;
+    }
 
 
     /**
